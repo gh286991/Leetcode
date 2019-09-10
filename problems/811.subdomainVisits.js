277ee4a8fcee4e1c3014af6cb40b31930d90843a
TomJhuang <gh286991@gmail.com>
2019-09-11 01:06:57 +0800

    subdomainVisits

--- problems/811.subdomainVisits.js
+++ problems/811.subdomainVisits.js
@@ -1,70 +1,36 @@
-const input =  ["9001 discuss.leetcode.com", "90 test.com"]
+const input =  ["9001 discuss.leetcode.com", "90 test.com" ,'100 yahoo.test.com']
 
 
 var subdomainVisits = function(cpdomains) { 
-    const ans = cpdomains.reduce((acc,curr,indx)=>{
+    const accObject = cpdomains.reduce((acc,curr,indx)=>{
         const splitArray = curr.split(' ')
         const vistTimes = Number(splitArray[0])
         const domains =splitArray[1].split('.').reverse()
 
-        console.log('curr' , splitArray)
-        console.log('domains' , domains)
-        console.log('vistTimes' , vistTimes)
-        
         let subDomains = ''
         for (let i = 0; i < domains.length; i++) {
-            subDomains += '.' +domains[i]
-            // console.log('subDomains' , subDomains)
+            if(i == 0){
+                subDomains =   domains[i] +subDomains
+            }else{
+                subDomains =  domains[i]  + '.' + subDomains
+            }
             
-            acc[subDomains] +=  vistTimes
+            let accPoint = 0
+                if (acc[subDomains] != undefined) {
+                accPoint = Number(acc[subDomains]);
+                }
+
+            acc[subDomains] = accPoint + vistTimes
             
         }
-        console.log('acc[subDomains]' , acc)
+
         return acc
-        // console.log('subDomains' , subDomains)
-        // return acc
     },{})
-
-
-
-    console.log(ans)
-    // const splitArray = cpdomains[0].split(' ')
-
-    // cpdomains.map((value,index)=>{
-
-    // const splitArray = value.split(' ')
-    // const domains =splitArray[1].split('.').reverse()
-    // const vistTimes = Number(splitArray[0])
-    // const reduce = domains.reduce((acc ,curr,indx)=>{
-
-    //     for (let i = 0; i < indx; i++) {
-    //         curr += '.' +domains[i]
-    //     }
-
-    //     console.log('curr' , curr)
-    //     acc[curr] = vistTimes
-    //    return  acc
-
-    // },{})
-
-    // console.log('reduce' , reduce)
-    // })
-    
-    // const domains =splitArray[1].split('.').reverse()
-    // const vistTimes = Number(splitArray[0])
-    // const reduce = domains.reduce((acc ,curr,indx)=>{
-
-    //     for (let i = 0; i < indx; i++) {
-    //         curr += '.' +domains[i]
-    //     }
-
-    //     console.log('curr' , curr)
-    //     acc[curr] = vistTimes
-    //    return  acc
-
-    // },{})
-
-    // console.log('reduce' , reduce)
+    const domains = Object.keys(accObject)
+    const ans = Object.values(accObject).map((v,index)=>{
+        return v+ ' ' +domains[index] 
+    })
+    return ans
 
 };
 
