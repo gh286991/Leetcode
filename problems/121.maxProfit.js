2b7c89dc4ccf24f161cafafe0fd89b18b71c55a5
Tom Jhuang <gh286991@gmail.com>
2019-09-27 11:44:26 +0800

    121 maxProfit soloved , #2

--- problems/121.maxProfit.js
+++ problems/121.maxProfit.js
@@ -1,33 +1,23 @@
 
-const input = [3,2,6,5,0,3]
+const input = []
 
 
 var maxProfit = function(prices) {
-    let min 
-    let minIndex
-    const filPrices = prices.filter( i => i != 0)
-    
-    min =  Math.min(...filPrices)
-    minIndex =  filPrices.indexOf(min)
 
-    if( filPrices.length === 0  ||filPrices.length === 1 ){
+    let min = Infinity
+
+    if(prices.length === 0){
         return 0
     }
 
-    if(minIndex === filPrices.length-1){
-        filPrices.splice( minIndex , 1)
-    }
+    const result = prices.map( value =>{
+        if(value < min ){
+            min = value
+        }
+        return  value - min
+    })
 
-    // console.log('prices' , prices)
-
-    min =  Math.min(...filPrices)
-    minIndex =  filPrices.indexOf(min)
-    prices.splice(0 , minIndex)
-
-    // console.log('prices' , prices)
-    const max = Math.max(...filPrices)
-
-    return  max-min
+    return Math.max(...result)
 };
 
 const ans = maxProfit(input)
