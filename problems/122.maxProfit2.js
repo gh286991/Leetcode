e1be04b79757a979eacc9d0aa89509a44100a2b7
Tom Jhuang <gh286991@gmail.com>
2019-09-27 14:48:54 +0800

    Sloved 122 Best time to buy and shell stock II , #4

---
+++ problems/122.maxProfit2.js
@@ -0,0 +1,23 @@
+const input = []
+
+var maxProfit = function(prices) {
+     
+    const benifit = prices.reduce( (acc , curr ,index)=>{
+
+        const nextValue = prices[index+1]
+
+        if( curr < nextValue ){
+            return  acc+  nextValue - curr
+        }
+
+        return acc
+    },0)
+
+    return benifit
+
+};
+
+
+const ans = maxProfit(input)
+
+console.log('ans ' , ans )
