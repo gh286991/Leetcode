3ce23eacdd99667b50cf4555b95c7a702ffbe54c
Tom Jhuang <gh286991@gmail.com>
2019-11-28 17:35:11 +0800

    update

--- problems/1222.queens-that-can-attack-the-king.py
+++ problems/1222.queens-that-can-attack-the-king.py
@@ -13,7 +13,6 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         queenLoc = {}
-        # print( 'd' , d['123'])
 
         for q in queens[0]:
             text = str(q).strip('[]')
@@ -25,8 +24,29 @@ class Solution(object):
         # maxBorder = 8
         for i in range(8):
             visited.append(False)
+        
+        step = 1
 
-        print(visited)
+        while( step < 8):
+            for idx ,d in enumerate(dirs):
+
+                if visited[idx] == False:
+                    x = king[0] + step*d[0]
+                    y = king[1] + step*d[1]
+
+                    if x < -1 or y<-1 or x >7 or y>7:
+                        visited[idx] = True
+                    else:
+                        pass
+                
+                print('x, y =(', x, y , ')')
+      
+                print('visited' , visited)
+                # print('x' , x)
+
+
+            step = step +1
+
 # @lc code=end
 queens = [[0,1],[1,0],[4,0],[0,4],[3,3],[2,4]], 
 king = [0,0]
