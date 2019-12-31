da0cba9dfec303b6ffd2c65851bac41fd6ae4993
Tom Jhuang <gh286991@gmail.com>
2019-12-31 13:34:22 +0800

    20.valid-parentheses ,#20

--- problems/20.valid-parentheses.js
+++ problems/20.valid-parentheses.js
@@ -9,8 +9,28 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    
+const isValid = function (s) {
+  const map = {
+    ')': '(',
+    '}': '{',
+    ']': '[',
+  };
+
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+      stack.push(s[i]);
+    } else if (map[s[i]] !== stack.pop()) {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
 };
 // @lc code=end
 
+const input = '{[]}';
+
+const ans = isValid(input);
+console.log(ans);
